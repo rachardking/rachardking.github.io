a65ba701639531bf7a62daa0e80bbc13b96a4b71
jinzhiwei <jinzhiwei@baidu.com>
2016-04-20 00:18:59 +0800

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp')
 var less = require('gulp-less');
 var myth = require('gulp-myth');
+var concat = require('gulp-concat');
 
 gulp.task('less', function () {
-    gulp.src('flex.less')
+    gulp.src('./lib/*.less')
         .pipe(less())
         .pipe(myth())
-        .pipe(gulp.dest('dist'));
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('css'));
 });
+
+gulp.task('default', ['less']);
